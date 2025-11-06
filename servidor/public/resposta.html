594a40b54adb5b62f9c26fc4da608921a07cb7e0
Weijun <davi.vickcaroli@gmail.com>
2025-11-06 16:11:19 -0300

    Bd

---
+++ servidor/public/resposta.html
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Resposta</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>Usuário cadastrado com sucesso!</h1>
+    </body>
+</html>
--- servidor/servidor.js
+++ servidor/servidor.js
@@ -2,12 +2,92 @@ require("colors");
 
 var http = require("http");
 var express = require("express");
+var bodyParser = require("body-parser")
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+const uri = `mongodb://localhost:27017`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
 
 var app = express();
-app.use(express.static("public"));
+app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', './views');
 
 var server = http.createServer(app);
-server.listen(3000);
+server.listen(80);
+
+console.log("Servidor rodando ...".rainbow);
+
+////////////////////////////////////////////// 
 
 
-console.log("Servidor Rodando...".rainbow);
+app.get('/', function(requisicao, resposta){
+    resposta.redirect('contato.html');
+})
+
+app.get('/cadastrar',function(requisicao, resposta){
+    console.log('Requisição recebida por get');
+
+    let nome = requisicao.query.nome;
+    let email = requisicao.query.email;
+    let telefone = requisicao.query.telefone;
+    let nascimento = requisicao.query.nascimento;
+    console.log(nome, email, telefone, nascimento)
+    resposta.render('resposta.ejs', {metodo:"GET", nome, email, telefone, nascimento});
+})
+
+app.post('/cadastrar',function(requisicao, resposta){
+    console.log('Requisição recebida por post');
+
+    let nome = requisicao.body.nome;
+    let email = requisicao.body.email;
+    let telefone = requisicao.body.telefone;
+    let nascimento = requisicao.body.nascimento;
+    console.log(nome, email, telefone, nascimento)
+    resposta.render('resposta.ejs', {metodo:"POST", nome, email, telefone, nascimento});
+})
+
+app.get('/for', function(requisicao, resposta){
+    let qtde = requisicao.query.qtde;
+    console.log(qtde);
+    resposta.render('for.ejs', {qtde})
+})
+
+///////////////////////////////////////////////////////////////
+
+var dbo = client.db("bd");
+var usuarios = dbo.collection("usuarios");
+
+
+ app.post("/cadastrar_usuario", function(req, resp) {
+    var data = {db_nome: req.body.nome, db_login: req.body.Login, db_senha: req.body.Senha };
+
+    usuarios.insertOne(data, function(err) {
+      if (err) {
+        resp.render('resposta_usuario', {resposta: "Erro ao Cadastrar!"})
+      }else {
+        resp.render('resposta_usuario', {resposta: "Usuário logado com sucesso!"})        
+      };
+    });
+
+  });
+
+app.post("/logar_usuario", function(req, resp) {
+    var data = {db_login: req.body.Login, db_senha: req.body.Senha };
+
+    usuarios.find(data).toArray(function(err, items) {
+      console.log(items);
+      if (items.length == 0) {
+        resp.render('resposta_usuario', {resposta: "Usuário/senha não encontrado!"})
+      }else if (err) {
+        resp.render('resposta_usuario', {resposta: "Erro ao logar usuário!"})
+      }else {
+        resp.render('resposta_usuario', {resposta: "Usuário logado com sucesso!"})        
+      };
+    });
+
+  });
